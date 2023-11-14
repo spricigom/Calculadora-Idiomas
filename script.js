ac95d76bbba0085cffe71b4d1d938fada899e192
spricigom <emilispricigo.ifc@gmail.com>
2023-11-14 16:54:38 -0300

    pronto

--- script.js
+++ script.js
@@ -1,28 +1,66 @@
-function clq1() {
-    alert ("voce clicou no botão 1");
-}
-
-let bt2 = document.getElementById("bt2");
-bt2.addEventListener("click", clq2);
-
-function clq2() {
-    alert ("voce clicou no botão 2");
-}
-let valor1 =document.querySelector("#valor1");
+let valor1 = document.querySelector("#valor1");
 let valor2 = document.querySelector("#valor2");
 let resultado = document.querySelector("#resultado");
 
-
-
-valor1.addEventListener("keyup", function(){
-    somar(Number(valor1.value), Number(valor2.value));
-});
-
-valor2.addEventListener("keyup", function(){
-    somar(Number(valor1.value), Number(valor2.value));
-});
-
+let btSoma = document.querySelector("#btSoma");
+btSoma.addEventListener("click", function(){
+    somar(Number(valor1.value),Number(valor2.value));
+})
 function somar(a, b) {
     let soma = a + b;
-    resultado.innerHTML = soma;    
+    resultado.innerHTML = soma;
 }
+
+let btSubtracao = document.querySelector("#btSubtracao");
+btSubtracao.addEventListener("click", function(){
+    subtrair(Number(valor1.value),Number(valor2.value));
+})
+function subtrair(a, b) {
+    let subtracao = a - b;
+    resultado.innerHTML = subtracao;
+}
+
+let btMultiplicacao = document.querySelector("#btMultiplicacao");
+btMultiplicacao.addEventListener("click", function(){
+    multiplicar(Number(valor1.value),Number(valor2.value));
+})
+function multiplicar(a, b) {
+    let multiplica = a * b;
+    resultado.innerHTML = multiplica;
+}
+
+let btDivisao = document.querySelector("#btDivisao ");
+btDivisao .addEventListener("click", function(){
+    dividir(Number(valor1.value),Number(valor2.value));
+})
+function dividir(a, b) {
+    let divide = a / b;
+    resultado.innerHTML = divide;
+}
+
+let title = document.querySelector("#title");
+
+let selectedLanguage = document.querySelector("#idioma");
+selectedLanguage.addEventListener("change", function(){
+    if(this.value == "br"){
+        btSoma.value = "Soma";
+        btSubtracao.value = "Subtração";
+        btMultiplicacao.value = "Multiplicação";
+        btDivisao.value = "Divisão";
+        title.textContent = "Calculadora"
+    }
+    else if (this.value == "en"){
+        btSoma.value = "Sum";
+        btSubtracao.value = "Subtraction";
+        btMultiplicacao.value = "Multiplication";
+        btDivisao.value = "Division";
+        title.textContent = "Calculator"
+    }
+    else if (this.value == "es"){
+        btSoma.value = "Suma";
+        btSubtracao.value = "Sustracción";
+        btMultiplicacao.value = "Multiplicación";
+        btDivisao.value = "División";
+        title.textContent = "Calculadora"
+    }
+})
